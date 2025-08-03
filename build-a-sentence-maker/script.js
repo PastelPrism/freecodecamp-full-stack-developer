e22f6163a7a2a35b8d4b9beac1ee7be7d79ccc24
PastelPrism <amy-van-leeuwen@proton.me>
2025-08-03 15:16:56 +0200

    Written out questions to answers

--- build-a-sentence-maker/script.js
+++ build-a-sentence-maker/script.js
@@ -1,19 +1,49 @@
+// 1. Declare an adjective and 7. assign a string to it
 let adjective = "blue";
+
+// 2. Declare a noun and 8. assign a string to it
 let noun = "monster";
+
+// 3. Declare a verb and 9. assign a string to it
 let verb = "happy";
+
+// 4. Declare a place and 10. assign a string to it
 let place = "cabin";
+
+// 5. Declare a second adjective and 11. assign a string to it
 let adjective2 = "red";
+
+// 6. Declare a second noun and 12. assign a string to it
 let noun2 = "wood";
 
+// 13. and 14. Declare a variable and use correct concatenation to create a story
 let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// 15. Log firstStory to the console
 console.log("First story: " + firstStory);
 
+// 17. Reassign adjective variable 
 adjective = "small";
+
+//18 Reassign noun variable
 noun = "dog";
+
+// 19. Reassign verb variable
 verb = "cold";
+
+// 20. Reassign place variable
 place = "parc";
+
+// 21. Reassign second adjective variable
 adjective2 = "big";
+
+// 22. Reassign second noun variable
 noun2 = "carrots";
 
+// 16. Declare a secondStory variable and 23. use correct format and concatenation 
 let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// 24. Log secondStory to the console
 console.log("Second story: " + secondStory);
+
+// 25. The first and second stories should be different 
