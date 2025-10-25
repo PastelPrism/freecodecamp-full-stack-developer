bc3359c9efc085749635cc87e1385dbbb6f28ba3
PastelPrism <amy-van-leeuwen@proton.me>
2025-10-25 23:28:52 +0200

    Added Celsius to Fahrenheit Converter assignment to the folder

---
+++ build-a-celsius-to-fahrenheit-converter/script.js
@@ -0,0 +1,8 @@
+// Create a function that converts Celsius to Fahrenheit
+
+function convertCtoF(celsius) { 
+    // Formula: (Celsius * 9/5) + 32
+  const fahrenheit = celsius * (9/5) + 32;
+  // Return Fahrenheit value
+  return fahrenheit;
+}
