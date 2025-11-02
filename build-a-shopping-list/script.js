c1b9269a9ffee793c54d1c29ce80d6ef836b0ffd
PastelPrism <amy-van-leeuwen@proton.me>
2025-11-02 23:57:09 +0100

    Added my solutions to Build a Shopping List assignment

---
+++ build-a-shopping-list/script.js
@@ -0,0 +1,26 @@
+console.log("Grocery shopping list");
+
+const shoppingList = [];
+
+console.log("It will be nice to have some fruit to eat.");
+
+shoppingList.push("Apples");
+
+function getShoppingListMsg(arr) {
+  return `Current Shopping List: ${arr}`;
+}
+
+console.log(getShoppingListMsg(shoppingList));
+
+shoppingList.push("Grapes");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("It looks like we need to get some cooking oil.");
+
+shoppingList.unshift("Vegetable Oil");
+console.log(getShoppingListMsg(shoppingList));
+
+shoppingList.push("Popcorn", "Beef Jerky", "Potato Chips");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("This looks like too much junk food.");
