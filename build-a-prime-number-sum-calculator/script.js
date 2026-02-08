660a7d19304ebcfb15cc6201c2383bbfd27f1e62
PastelPrism <amy-van-leeuwen@proton.me>
2026-02-08 23:49:43 +0100

    Added Build a Prime Number Sum Calculator assignment to the long long list

---
+++ build-a-prime-number-sum-calculator/script.js
@@ -0,0 +1,15 @@
+function sumPrimes(number) {
+  function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+
+  let sum = 0;
+  for (let i = 2; i <= number; i++) {
+    if (isPrime(i)) sum += i;
+  }
+  return sum;
+}
